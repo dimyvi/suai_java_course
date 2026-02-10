f190d382a288f3161b7642248e5b7634b1f0ff9e
Dmitry <dimyvybornov@gmail.com>
2026-02-10 14:20:21 +0300

    Initial commit

---
+++ first_semester/int_lab1.java
@@ -0,0 +1,47 @@
+class Int{
+    private int number = 0;
+
+    public void increment(){
+        number += 1;
+    }
+
+    public void decrement(){
+        number -= 1;
+    }
+
+    public void add(Int n){
+        number += n.number;
+    } 
+
+    public void substract(Int n){
+        number -= n.number;
+    }
+
+    public String toString(){
+        return Integer.toString(number);
+    }
+}
+
+public class int_lab1{
+    public static void main (String args[]){
+        Int a = new Int();
+        Int b = new Int();
+        Int c = new Int();
+
+        b.increment();
+
+        for (int i = 0; i < 10; i++) {
+            b.add(b);
+        }
+
+        a.add(b);
+
+        for (int i = 0; i < 24; i++){
+            c.increment();
+        }
+
+        a.substract(c);
+
+        System.out.println(a);
+    }
+}
---
+++ first_semester/matrix_lab2.java
@@ -0,0 +1,21 @@
+class Matrix{
+    private int n;
+    private int[][] mtx;
+
+    public Matrix(int size){
+        n = size;
+        mtx = new int[n][n];
+    }
+
+
+
+}
+
+public class matrix_lab2{
+    public static void main (String args[]){
+        Matrix mx1 = new Matrix(3);
+        for(int i = 0; i < 3; i++){
+            System.out.println(Matrix[i]);
+        }
+    }
+}
